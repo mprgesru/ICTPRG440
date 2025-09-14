0aa22ec47be1344f03bd656b9e1da4e2dbb8a551
mprgesru <132899521+mprgesru@users.noreply.github.com>
2025-09-14 23:39:46 +0930

    Task 2
    This programme defines and checks a GeoDataFrame, verifies the original CRS,
    and converts the coordinate reference system (CRS) from WGS84 to GDA2020 Zone 56.
    It also extracts each country and exports the results as GeoJSON files.
    
    The shape file has been added here. Please change the FILE_PATH and OUTPUT_PATH before you run the programme. You have to create a folder "Output" to store the extracted GeoJSON files.

---
+++ Masud Scripts/main.py
@@ -0,0 +1,86 @@
+import geopandas as gpd
+
+
+""" 
+Programme Objective: This programme defines and checks a GeoDataFrame, verifies the original CRS,
+and converts the coordinate reference system (CRS) from WGS84 to GDA2020 Zone 56. 
+It also extracts each country and exports the results as GeoJSON files.
+
+Programme created by: Md Masud Parves Rana
+"""
+
+
+FILE_PATH = r"C:\440_Assessment\WorldCities\LocationCities.shp"
+OUTPUT_PATH = r"C:\440_Assessment\WorldCities\Output"
+
+# 1. Loading data from the source shapefile into geodataframe
+
+gdf =gpd.read_file(FILE_PATH)
+
+if isinstance(FILE_PATH,str):     # This helps to check File_path is a string or not string
+       print("It is a string")
+else:
+    raise ValueError("Not a string")
+
+
+# 2. Checking the Geodataframe (Attribute Table) and Geometry
+
+print(gdf.head())
+
+# 3. Checking all the columns in the table
+
+print (gdf.columns)
+
+# 4. Checking the Original CRS of the source shape file
+
+print("Original CRS:", gdf.to_crs)
+
+# 5. Reprojection
+
+def transfrom_WGS84_To_GDA2020(File_Path,ESPG=7856):
+
+    """
+    This function transform coordinates from WGS84 to Another
+    """
+    
+    try:
+       gdf = gpd.read_file(File_Path)
+       gdf = gdf.to_crs(epsg=ESPG)
+    except Exception as e:    # Try and Except helps to identify errors/causes of crashing
+
+     print("This is wrong", e)
+
+    return gdf # return a geodataframe object
+
+transformedgdf =transfrom_WGS84_To_GDA2020(FILE_PATH) # transformed to GDA2020 Z56
+
+print("Transformed CRS:", transformedgdf.to_crs)
+
+#print(transformedgdf.head())
+
+# 6. Showing the list of all countries from the original shapefile
+
+print(transformedgdf["Country"].unique())
+
+# 7. Extracting Countrywise Cities
+
+def Extract_Cities_by_Country(data, Country="Country"):
+    Country_gdf=transformedgdf[transformedgdf["Country"]==Country]   # this is for filtering out
+    outputfile=OUTPUT_PATH+"\\"+f"{Country}.geojson"                 # exporting file name in output
+    Country_gdf.to_file(outputfile)                                  # saving as GeojSON to output after filtering
+
+
+""" 
+   - This function takes gdf and returns a gdf with only data from the specific Country
+   - It takes Output folder as a string parameter
+   - This function export the file as output.geojson
+"""
+
+# 8. For Australia
+
+Extract_Cities_by_Country(data=gdf,Country="Australia")
+
+# 9. For Bangladesh
+
+Extract_Cities_by_Country(data=gdf,Country="Bangladesh")
+
